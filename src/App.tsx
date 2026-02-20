580a635e9a0050d27f5f8120f9cd39638f860399
ashar-codes <muhammadashar0300@gmail.com>
2026-02-20 17:39:15 +0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import GeminiSlingshot from './components/GeminiSlingshot'
+
+export default function App() {
+  return (
+    <div className="w-full h-screen">
+      <GeminiSlingshot />
+    </div>
+  )
+}
